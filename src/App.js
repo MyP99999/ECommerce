caa9085b48b3d9f5f886ace488c0765cb1d904be
MyP99999 <matheosprofirescu@yahoo.com>
2022-12-02 14:30:12 +0200

    default page

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           <Route exact path='/' component={ProductList} />
           <Route path='/details' component={Details} />
           <Route path='/cart' component={Cart} />
-          <Route Component={Default} />
+          <Route component={Default} />
         </Switch>
         <Modal />
       </React.Fragment>
