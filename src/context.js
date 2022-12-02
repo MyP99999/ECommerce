06163023dc237c67b465df360f435e0342db7068
MyP99999 <matheosprofirescu@yahoo.com>
2022-12-02 14:01:19 +0200

    remove item

--- src/context.js
+++ src/context.js
@@ -83,7 +83,25 @@ class ProductProvider extends Component {
       }
 
       removeItem = (id) =>{
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
 
+        tempCart = tempCart.filter(item=> item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(()=>{
+          return{
+            cart:[...tempCart],
+            product:[...tempProducts]
+          }
+        },()=>{
+          this.addTotals();
+        })
       }
 
       clearCart = () =>{
